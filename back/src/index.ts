8abce0aadb855ebafe67504f96ada4451ec2d483
ManiCH06 <lqmanik@gmail.com>
2025-02-03 18:54:08 +0100

    Please

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
 import express from "express";
 import router from "./app";
 import cors from "cors"; // Import du module cors pour
+import path from "path";
 import cookieParser from "cookie-parser"; // pour lire les cookies dans les requêtes
 
+dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
+console.log(process.cwd());
 const app = express();
 const PORT = process.env.BACK_PORT || 5000;
 
@@ -10,8 +14,7 @@ app.use(cookieParser());
 // Ajouter les middlewares globaux
 app.use(
   cors({
-    origin: "https://www.ezplace.tech/",
-    /* origin: process.env.FRONT_URL, */
+    origin: process.env.FRONT_URL,
     credentials: true,
   })
 );
