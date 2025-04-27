7a7d4ca9752697e52aef68b6ad8b08c013aa3461
ManiCH06 <lqmanik@gmail.com>
2025-04-27 23:50:28 +0200

    Test SSR for app directory next

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,6 @@
+// app/page.tsx
+
+import { Boutique, Product } from "@/@types/types";
 import CardShopSection from "@/components/pages/home/CardShopSection";
 import PresentationSection from "@/components/pages/home/PresentationSection";
 import FilterForm from "@/components/pages/home/FilterForm";
@@ -5,45 +8,17 @@ import HeroSection from "@/components/pages/home/HeroSection";
 import CategoryShowcase from "@/components/pages/home/CategorySowcase";
 import ProductSlider from "@/components/pages/home/ProductSlider";
 
-import { Boutique } from "@/@types/types";
-import axios from "axios";
-import { Product } from "@/@types/types";
+export default async function Home() {
 
-export async function getServerSideProps() {
-  try {
-    const API_URL = process.env.API_URL;
+  const [boutiquesRes, productsRes] = await Promise.all([
+    fetch(`/api/shops/promoteshops`),
+    fetch(`/api/products`),
+  ]);
 
-    const [boutiquesRes, productsRes] = await Promise.all([
-      axios.get(`${API_URL}/shops/promoteshops`, { timeout: 5000 }),
-      axios.get(`${API_URL}/products`, { timeout: 5000 }),
-    ]);
+  // Parse responses
+  const boutiques: Boutique[] = await boutiquesRes.json();
+  const products: Product[] = await productsRes.json();
 
-    return {
-      props: {
-        boutiques: boutiquesRes.data,
-        products: productsRes.data,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching data:", error);
-
-    // Return empty arrays or a safe fallback in case of error
-    return {
-      props: {
-        boutiques: [],
-        products: [],
-      },
-    };
-  }
-}
-
-export default function Home({
-  boutiques,
-  products,
-}: {
-  boutiques: Boutique[];
-  products: Product[];
-}) {
   return (
     <div>
       <header className="text-center py-8 transition-transform duration-300">
