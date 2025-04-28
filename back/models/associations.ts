c20b616fb664af55e2708959a3cc2d877c69bd82
ManiCH06 <lqmanik@gmail.com>
2025-04-28 22:59:02 +0200

    HOTFIX: Fixed typo

--- back/models/associations.ts
+++ back/models/associations.ts
@@ -6,42 +6,61 @@ import Order from "./Order.js";
 import OrderLine from "./OrderLine.js";
 import Review from "./Review.js";
 
-//User
-User.belongsTo(Role, { foreignKey: "roleId", as: "role" });
-Role.hasMany(User, { foreignKey: "roleId", as: "users" });
+export default function associate(models: Record<string, any>) {
+  //User
+  models.User.belongsTo(models.Role, { foreignKey: "roleId", as: "role" });
+  models.Role.hasMany(models.User, { foreignKey: "roleId", as: "users" });
 
-User.hasMany(Order, { foreignKey: "userId", as: "orders" });
+  models.User.hasMany(models.Order, { foreignKey: "userId", as: "orders" });
 
-//Shop
-Shop.belongsTo(User, { foreignKey: "userId", as: "user" });
-User.hasMany(Shop, { foreignKey: "userId", as: "shops" });
+  //Shop
+  models.Shop.belongsTo(models.User, { foreignKey: "userId", as: "user" });
+  models.User.hasMany(models.Shop, { foreignKey: "userId", as: "shops" });
 
-Shop.hasMany(Product, { foreignKey: "shopId", as: "products" });
-Product.belongsTo(Shop, { foreignKey: "shopId", as: "shop" });
+  models.Shop.hasMany(models.Product, { foreignKey: "shopId", as: "products" });
+  models.Product.belongsTo(models.Shop, { foreignKey: "shopId", as: "shop" });
 
-//Order
-Order.belongsTo(User, { foreignKey: "userId", as: "user" });
-User.hasMany(Order, { foreignKey: "userId", as: "orders" });
+  //Order
+  models.Order.belongsTo(models.User, { foreignKey: "userId", as: "user" });
+  models.User.hasMany(models.Order, { foreignKey: "userId", as: "orders" });
 
-//OrderLine
-OrderLine.belongsTo(Order, { foreignKey: "orderId", as: "order" });
-Order.hasMany(OrderLine, { foreignKey: "orderId", as: "orderLines" });
+  //OrderLine
+  models.OrderLine.belongsTo(models.Order, {
+    foreignKey: "orderId",
+    as: "order",
+  });
+  models.Order.hasMany(models.OrderLine, {
+    foreignKey: "orderId",
+    as: "orderLines",
+  });
 
-OrderLine.belongsTo(Product, { foreignKey: "productId", as: "product" });
-Product.hasMany(OrderLine, { foreignKey: "productId", as: "orderLines" });
+  models.OrderLine.belongsTo(models.Product, {
+    foreignKey: "productId",
+    as: "product",
+  });
+  models.Product.hasMany(models.OrderLine, {
+    foreignKey: "productId",
+    as: "orderLines",
+  });
 
-//Product
-Product.hasMany(OrderLine, { foreignKey: "productId", as: "orderLines" });
+  //Product
+  models.Product.hasMany(models.OrderLine, {
+    foreignKey: "productId",
+    as: "orderLines",
+  });
 
-//Review
-Review.belongsTo(User, { foreignKey: "userId", as: "user" });
-User.hasMany(Review, { foreignKey: "userId", as: "reviews" });
+  //Review
+  models.Review.belongsTo(models.User, { foreignKey: "userId", as: "user" });
+  models.User.hasMany(models.Review, { foreignKey: "userId", as: "reviews" });
 
-Review.belongsTo(Product, { foreignKey: "productId", as: "product" });
-Product.hasMany(Review, { foreignKey: "productId", as: "reviews" });
+  models.Review.belongsTo(models.Product, {
+    foreignKey: "productId",
+    as: "product",
+  });
+  models.Product.hasMany(models.Review, {
+    foreignKey: "productId",
+    as: "reviews",
+  });
 
-
-
-export default function associate() {
   console.log("Associations done!");
 }
